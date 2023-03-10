abc220f598343efaf5ffc24f3f740467bb1629ab
Nikhil-R-B <bnikhil159@gmail.com>
2023-03-10 18:16:20 +0000

    Read and write logic added to data copier

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+import os
+import sys
+from read import get_json_reader
+from write import load_db_table
+
+
+def process_table(BASE_DIR, conn, table_name):
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for df in json_reader:
+        load_db_table(df, conn, table_name, df.columns[0])
+
+
 def main():
-    print("Hello World")
+    BASE_DIR = os.environ.get('BASE_DIR')
+    # table_name = os.environ.get('TABLE_NAME')
+    table_names = sys.argv[1].split(',')
 
-# Use pip install -r requirements.txt to install using requirements file
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        process_table(BASE_DIR, conn, table_name)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
